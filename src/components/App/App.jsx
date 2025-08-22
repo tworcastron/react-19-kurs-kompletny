eeb314dd41569f3eb66554da3ffd5dbf01b32082
tworcastron <adam@tworcastron.pl>
2025-08-22 10:54:28 +0200

    komponenty bazowe

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import Header from '../Header/Header'
+import Hotels from '../Hotels/Hotels'
+import Menu from '../Menu/Menu'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Menu />
+      <Hotels />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hotels/Hotels.jsx
@@ -0,0 +1,13 @@
+import Hotel from "./Hotel/Hotel"
+
+const Hotels = () => {
+  return (
+    <div>
+      <Hotel />
+      <Hotel />
+      <Hotel />
+    </div>
+  )
+}
+
+export default Hotels 
