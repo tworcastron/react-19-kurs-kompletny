5cb09a6609018c048b97bbf970123b4e8328c61c
tworcastron <adam@tworcastron.pl>
2025-09-04 19:31:03 +0200

    useMemo

--- src/components/Hotels/Hotels.jsx
+++ src/components/Hotels/Hotels.jsx
@@ -1,14 +1,29 @@
-import { memo } from "react"
+import { memo, useMemo } from "react"
 import Hotel from "./Hotel/Hotel"
 
+// const slowFunction = (count) => {
+//   console.log('start')
+//   for (let i = 0; i < 2500000000; i++) {}
+//   console.log('koniec')
+//   return count
+// }
+
 const Hotels = (props) => {
+  // wykonuje obliczenia ponownie tylko gdy props.hotels.length się zmienia 
+  // const count = useMemo(
+  //   () => slowFunction(props.hotels.length),
+  //   [props.hotels.length]
+  // )
+
+  const count = props.hotels.length
+
   return (
     <div>
       <div style={{
         border: '1px solid #cdcdcd',
         padding: 10,
       }}>
-        <h2>Oferty:</h2>
+        <h2>Oferty ({ count }):</h2>
         {props.hotels.map((hotel) => (
           <Hotel {...hotel} key={hotel.id} />
         ))}
