ad15d710d5e1054efff0719f2538b9604d350ca2
tworcastron <adam@tworcastron.pl>
2025-09-04 16:54:39 +0200

    paralax

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,17 @@ import withMousePosition from '../../hoc/withMousePosition'
 import styles from './Header.module.scss'
 
 function Header(props) {
+  const paralaxStyle = {
+    transform: `translate(
+      ${props.mouseX / -50}px,
+      ${props.mouseY / 120}px
+    )`
+  }
+
   return (
     <div className={`${styles.header}`}>
+      <div className={styles.headerImage} style={paralaxStyle} />
       <div>
-        {props.mouseX} {props.mouseY}
         {props.children}
       </div>
     </div>
