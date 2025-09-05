4f97a332631fc9145e92a0f52236f334f0924391
tworcastron <adam@tworcastron.pl>
2025-09-05 13:16:15 +0200

    po co nam hooki

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,20 @@ import App from './components/App/App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
+// useState()
+
+function TestHook() {
+  return (
+    <>
+      <h1>Test hook!</h1>
+      <input type='text' />
+    </>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TestHook />
+    {/* <App /> */}
   </StrictMode>,
 )
