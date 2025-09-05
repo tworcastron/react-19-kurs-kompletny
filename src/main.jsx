1c93a3d97fdc46fa33f440b02acfb8d2cc3572bb
tworcastron <adam@tworcastron.pl>
2025-09-05 14:13:59 +0200

    wlasny useState

--- src/main.jsx
+++ src/main.jsx
@@ -5,20 +5,53 @@ import App from './components/App/App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
-// useState()
+let globalState = []
+let idx = 0
+// console.log('init', globalState, idx)
+
+function useState(initValue) {
+  const id = idx++
+
+  if (globalState[id]) return globalState[id]
+
+  const setValue = (val) => {
+    globalState[id][0] = val
+    // console.log('set', globalState, idx)
+    render()
+  }
+  // console.log('get', globalState, idx)
+
+  const state = [initValue, setValue]
+  globalState[id] = state
+
+  return state
+}
 
 function TestHook() {
+  const [input, setInput] = useState('start')
+  const [text, setText] = useState('drugi stan')
   return (
     <>
       <h1>Test hook!</h1>
-      <input type='text' />
+      <input type='text' 
+        value={input} onChange={e => setInput(e.target.value)} />
+      <input type='text' 
+        value={text} onChange={e => setText(e.target.value)} />
+      <p>{input}</p>
+      <p>{text}</p>
     </>
   )
 }
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <TestHook />
-    {/* <App /> */}
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <TestHook />
+//     {/* <App /> */}
+//   </StrictMode>,
+// )
+const root = createRoot(document.getElementById('root'))
+function render() {
+  idx = 0
+  root.render(<TestHook />)
+}
+render()
