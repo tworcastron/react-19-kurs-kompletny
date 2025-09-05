352b547c50568cfc0b2649a242b215804c658b50
tworcastron <adam@tworcastron.pl>
2025-09-05 14:38:48 +0200

    useDebugValue

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,8 +1,11 @@
-import { useContext } from "react"
+import { useContext, useDebugValue } from "react"
 import AuthContext from "../context/AuthContext"
 
 export default function useAuth() {
   const authContext = useContext(AuthContext)
+  const user = authContext.isAuthenticated
+
+  useDebugValue(user, (user) => user ? 'Zalogowany' : 'Wylogowany')
 
   const setUser = (value) => {
     if (value) {
@@ -12,5 +15,5 @@ export default function useAuth() {
     }
   }
 
-  return [authContext.isAuthenticated, setUser]
+  return [user, setUser]
 }
