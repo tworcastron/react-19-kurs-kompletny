67857daee03ccf59b14ea7bdddffbd032b0f3fc6
tworcastron <adam@tworcastron.pl>
2025-09-13 11:19:50 +0200

    formularz dodawania hotelu - logika

--- src/pages/MyHotels/HotelForm.jsx
+++ src/pages/MyHotels/HotelForm.jsx
@@ -1,21 +1,111 @@
+import { useState } from "react";
 import Input from "../../components/UI/Input/Input";
 
 export default function HotelForm() {
+  const [form, setForm] = useState({
+    title: {
+      value: 'nazwa',
+      error: '',
+      rules: ['required']
+    },
+    city: {
+      value: '',
+      error: '',
+      rules: ['required']
+    },
+    description: {
+      value: '',
+      error: '',
+      rules: ['required']
+    },
+    rooms: {
+      value: '2',
+      error: '',
+      rules: ['required']
+    },
+    file: {
+      value: '',
+      error: '',
+      rules: ['required']
+    },
+    status: {
+      value: 0,
+      error: '',
+      rules: ['required']
+    },
+    features: {
+      value: ['tv'],
+      error: '',
+    },
+  })
+  const validate = () => ''
+  const onInputChange = (field, value) => {
+    const newValue = {
+      ...form[field],
+      value,
+      error: validate(form[field].rules, value)
+    }
+    setForm({ ...form, [field]: newValue })
+  }
+
   return (
     <>
-      <Input label="Nazwa" error="coś nie tak" />
-      <Input label="Miejscowość"  />
-      <Input label="Opis" type="textarea" />
-      <Input label="Zdjęcie" type="file" />
-      <Input label="Status" type="select" options={[
-        { value: 1, label: 'Aktywny' },
-        { value: 0, label: 'Ukryty' }
-      ]} />
-      <Input label="Udogodnienia" type="checkbox" options={[
-        { value: 'parking', label: 'tv' },
-        { value: 'parking', label: 'wifi' },
-        { value: 'parking', label: 'parking' }
-      ]} />
+      <Input 
+        label="Nazwa"
+        value={form.title.value}
+        error={form.title.error}
+        onChange={value => onInputChange('title', value)}
+      />
+
+      <Input
+        label="Miejscowość"
+        value={form.city.value}
+        error={form.city.error}
+        onChange={value => onInputChange('city', value)}
+      />
+      <Input
+        label="Opis"
+        type="textarea"
+        value={form.description.value}
+        error={form.description.error}
+        onChange={value => onInputChange('description', value)}
+      />
+      <Input 
+        label="Ilość pokoi"
+        value={form.rooms.value}
+        error={form.rooms.error}
+        onChange={value => onInputChange('rooms', value)}
+      />
+      <Input
+        label="Zdjęcie"
+        type="file"
+        value={form.file.value}
+        error={form.file.error}
+        onChange={value => onInputChange('file', value)}
+      />
+      <Input 
+        label="Status"
+        type="select"
+          options={[
+          { value: 1, label: 'Aktywny' },
+          { value: 0, label: 'Ukryty' }
+        ]}
+        value={form.status.value}
+        error={form.status.error}
+        onChange={value => onInputChange('status', value)}
+      />
+      <Input
+        label="Udogodnienia"
+        type="checkbox"
+          options={[
+          { value: 'tv', label: 'tv' },
+          { value: 'wifi', label: 'wifi' },
+          { value: 'parking', label: 'parking' }
+        ]}
+        value={form.features.value}
+        error={form.features.error}
+        onChange={value => onInputChange('features', value)}
+      />
     </>
   )
 }
