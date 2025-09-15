ca56fa3fdba7ecc2fb83317ca31b295c3a28c1bd
tworcastron <adam@tworcastron.pl>
2025-09-15 11:23:31 +0200

    pobieranie hotelu

--- src/pages/HotelPreview.jsx
+++ src/pages/HotelPreview.jsx
@@ -2,21 +2,25 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import LoadingIcon from "../components/UI/LoadingIcon/LoadingIcon"
 import { initHotels } from "../reducer"
+import axios from "../axios"
 
 export default function HotelPreview() {
   const { id } = useParams()
   const [hotel, setHotel] = useState(null)
 
   useEffect(() => {
-    // pobieranie danych
-    setTimeout(() => {
-      setHotel(initHotels.find(x => x.id === Number(id)))
-    }, 500)
+    const fetchHotel = async () => {
+      const res = await axios.get(`/hotels/${id}.json`)
+      console.log(res.data)
+      setHotel(res.data)
+    }
+    
+    fetchHotel()
   }, [id])
 
   if (!hotel) return <LoadingIcon />
 
   return (
-    <h1>Hotel: {hotel.name}</h1>
+    <h1>Hotel: {hotel.title}</h1>
   )
 }
